b18a0f5332e2b28fa1597f3a6ef0121dd9dd6913
sumaiya48 <sumaiyaislam202021@gmail.com>
2025-05-30 12:40:42 +0600

    msg

--- app/page.js
+++ app/page.js
@@ -1,16 +1,15 @@
 import Image from "next/image";
-import Policy from "./policy";
 import Banner from "./components/Banner";
 import Footer from "./Footer";
 import LatestCollections from "./LatestCollections";
-import ProductCard from "./components/ProductCard";
+import Policy from "./Policy";
 
 export default function Home() {
   return (
     <div className="">
       <Banner></Banner>
       <LatestCollections></LatestCollections>
-     <Policy></Policy>
+      <Policy></Policy>
      <Footer></Footer>
     </div>
   );
